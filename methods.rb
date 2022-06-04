f60d97e55f7d614b3c87eb5d9c7302140e837663
Benjamin <shallifi@hotmail.com>
2022-06-04 11:27:35 -0400

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def my_method(param)
+    puts "Running my_method"
+    param + 1
+end
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve num1
+    num1/2 if num1.is_a? Integer
+end
+
+#example of writing the same thing in a different way
+# if num1.is_a? Integer 
+# num1/2
+# end
